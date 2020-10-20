bc4b2f2b779c34b0085f7313259d5137e2e93ea7
XiaoTuGou <296404875@qq.com>
2020-10-20 15:21:15 +0800

    Update LogViewer.php

--- src/Components/LogViewer.php
+++ src/Components/LogViewer.php
@@ -15,7 +15,7 @@ class LogViewer extends Component
         $this->routers = [
             'get_chart_data' => route('log_view_get_chart_data',[],false),
             'get_list_logs' => route('log_view_get_list_logs',[],false),
-            'download' => route('log_view_download', ['date' => '##date##'],[],false),
+            'download' => route('log_view_download', ['date' => '##date##'],false),
             'get' => route('log_view_get', ['date' => '##date##', 'level' => '##level##'],false),
             'delete' => route('log_view_delete',[],false),
         ];
