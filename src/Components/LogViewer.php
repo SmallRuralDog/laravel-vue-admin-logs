fbe34492293cc747d485d39b408cf557567a87de
XiaoTuGou <296404875@qq.com>
2020-10-20 15:20:26 +0800

    Update LogViewer.php

--- src/Components/LogViewer.php
+++ src/Components/LogViewer.php
@@ -13,11 +13,11 @@ class LogViewer extends Component
     public function __construct()
     {
         $this->routers = [
-            'get_chart_data' => route('log_view_get_chart_data'),
-            'get_list_logs' => route('log_view_get_list_logs'),
-            'download' => route('log_view_download', ['date' => '##date##']),
-            'get' => route('log_view_get', ['date' => '##date##', 'level' => '##level##']),
-            'delete' => route('log_view_delete'),
+            'get_chart_data' => route('log_view_get_chart_data',[],false),
+            'get_list_logs' => route('log_view_get_list_logs',[],false),
+            'download' => route('log_view_download', ['date' => '##date##'],[],false),
+            'get' => route('log_view_get', ['date' => '##date##', 'level' => '##level##'],false),
+            'delete' => route('log_view_delete',[],false),
         ];
     }
 
